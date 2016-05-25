86ed6351bf9523bd8775bb7cae9bca8e105b8317
Seth Lytton <slytton@gmail.com>
2016-05-24 19:18:58 -0600

    Fix error understandings seed file.

--- server/seeds/06_understandings.js
+++ server/seeds/06_understandings.js
@@ -13,14 +13,15 @@ exports.seed = function(knex, Promise) {
       insertUnderstandings('Angular Stuff', "I don't get it", 'Bob', new Date(Date.now()+15000)),
       insertUnderstandings('Angular Stuff', 'I get it', 'Bob', new Date(Date.now()+200000)),
       insertUnderstandings('Angular Stuff', "I don't get it", 'Bob', new Date(Date.now()+350000)),
+
       insertUnderstandings('Sockets Stuff', 'Undecided', 'Dave', new Date()),
       insertUnderstandings('Sockets Stuff', "I don't get it", 'Dave', new Date(Date.now()+10000)),
-      insertUnderstandings('Sockets Stuff', 'I get it', 'Dave', new Date(Date.now()+50000)),
-      insertUnderstandings('Sockets Stuff', "I don't get it", 'Dave', new Date(Date.now()+70000)),
+      insertUnderstandings('Sockets Stuff', 'I get it', 'Dave', new Date(Date.now()+900000)),
+      insertUnderstandings('Sockets Stuff', "I don't get it", 'Dave', new Date(Date.now()+1000000)),
       insertUnderstandings('Sockets Stuff', 'Undecided', 'Bob', new Date()),
-      insertUnderstandings('Sockets Stuff', "I don't get it", 'Bob', new Date(Date.now()+15000)),
-      insertUnderstandings('Sockets Stuff', 'I get it', 'Bob', new Date(Date.now()+200000)),
-      insertUnderstandings('Sockets Stuff', "I don't get it", 'Bob', new Date(Date.now()+350000))
+      insertUnderstandings('Sockets Stuff', "I don't get it", 'Bob', new Date(Date.now()+234000)),
+      insertUnderstandings('Sockets Stuff', 'I get it', 'Bob', new Date(Date.now()+5300000)),
+      insertUnderstandings('Sockets Stuff', "I don't get it", 'Bob', new Date(Date.now()+4340000))
     );
   });
   function insertUnderstandings(lectureName, status, userName, timestamp){
